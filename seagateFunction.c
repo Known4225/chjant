1ac4df36ef0ff9c64f0cdeab8994b9d8e1d076ec
Ryan Srichai <ryansrichai@gmail.com>
2023-09-25 16:54:21 -0500

    CSELabsUpdate

--- seagateFunction.c
+++ seagateFunction.c
@@ -1,6 +1,6 @@
 unsigned char seagate(unsigned char input1, unsigned char input2) {
     /* example code */
-    unsigned char sum = input1 == input2;
+    unsigned char sum = input1 ^ input2;
     unsigned char carry = sum & input2;
     unsigned char cong = ((sum & input1) | (carry | input2)) ^ (carry & carry);
     sum = cong ^ 9283;
