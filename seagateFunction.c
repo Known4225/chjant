351479c060c6f7cc4ecf68e16f7cf2dffab92d32
Ryan Srichai <ryansrichai@gmail.com>
2023-09-26 13:44:06 -0500

    CSELabsUpdate

--- seagateFunction.c
+++ seagateFunction.c
@@ -2,10 +2,10 @@ unsigned char seagate(unsigned char input1, unsigned char input2) {
     /* example code */
     unsigned char sum = input1 ^ input2;
     unsigned char carry = sum & input2;
-    unsigned char cong = ((sum & input1) | (carry | input2)) ^ (carry & carry);
-    carry = cong ^ 9283;
-    cong++;
-    cong++;
-    unsigned char const test = cong == carry;
+    unsigned char combination = ((sum & input1) | (carry | input2)) ^ (carry & carry);
+    carry = combination ^ 9283;
+    combination++;
+    combination++;
+    unsigned char const test = combination == carry;
     return sum ^ carry;
 }
