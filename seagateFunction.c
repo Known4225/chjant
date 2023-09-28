7a67a3ad55b994031b427190657aee960147c621
Ryan Srichai <ryansrichai@gmail.com>
2023-09-28 09:22:19 -0500

    genericUpdate

--- seagateFunction.c
+++ seagateFunction.c
@@ -3,5 +3,5 @@ unsigned char seagate(unsigned char input1, unsigned char input2) {
     unsigned char a = input1;
     unsigned char b = input2;
     unsigned char c = 15;
-    return a + -b;
+    return -a + -(b + a);
 }
--- seagateFunction_out.c
+++ seagateFunction_out.c
@@ -1,1 +1,1 @@
-unsigned char seagate(unsigned char input1,unsigned char input2){unsigned char a=input1;unsigned char b=input2;unsigned char c=15;return a+-b;}
+unsigned char seagate(unsigned char input1,unsigned char input2){unsigned char a=input1;unsigned char b=input2;unsigned char c=15;return-a+-(b+a);}
