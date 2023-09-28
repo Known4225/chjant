a7e522e661f01ee67343657d7a5b9513a0909af7
Ryan Srichai <ryansrichai@gmail.com>
2023-09-28 12:52:38 -0500

    CSELabsUpdate

--- seagateFunction_out.c
+++ seagateFunction_out.c
@@ -1,1 +1,1 @@
-unsigned char seagate(unsigned char input1,unsigned char input2){return(input1+input1)-(input2+input2);}
+unsigned char seagate(unsigned char input1,unsigned char input2){return(input2+input2+input2)-(input1+input1);}
